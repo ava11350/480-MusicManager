73a052813debbf7c276bf1e6d2a094dea9ba0d3b
Ava Smith <ava.smith103@gmail.com>
2020-12-10 21:09:09 -0600

    Added combo box and UI adjustment

--- MusicManager/src/main/java/UserScene.java
+++ MusicManager/src/main/java/UserScene.java
@@ -1,6 +1,9 @@
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
@@ -20,24 +23,50 @@ public class UserScene {
     }
 
     void initScene(){
+        ObservableList<String> recTypes = FXCollections.observableArrayList(
+                "Year",
+                "Artist",
+                "Acousticness",
+                "Danceability",
+                "Energy",
+                "Instrumentalness",
+                "Liveness",
+                "Speechiness",
+                "Tempo",
+                "Valence",
+                "I'm Feeling Groovy"
+        );
+
         TextField artistSearch = new TextField();
         TextField songSearch = new TextField();
         TextField yearSearch = new TextField();
+        ComboBox comboBox = new ComboBox(recTypes);
         searchButton = new Button("Search");
         artistSearch.setPromptText("Artist");
         songSearch.setPromptText("Song Name");
         yearSearch.setPromptText("Year");
         VBox searchTextBox = new VBox(artistSearch, songSearch, yearSearch);
-        HBox searchField = new HBox(searchTextBox, searchButton);
+        searchTextBox.setSpacing(10);
+        HBox searchField = new HBox(searchTextBox, comboBox, searchButton);
+        searchField.setSpacing(10);
+        searchField.setAlignment(Pos.CENTER);
         ListView<String> searchResults = new ListView<String>();
+        searchResults.setPrefWidth(400);
         ListView<String> recommendationResults = new ListView<String>();
+        recommendationResults.setPrefWidth(400);
         HBox displayArea = new HBox(searchResults, recommendationResults);
+        displayArea.setPrefWidth(1000);
+        displayArea.setSpacing(50);
+        displayArea.setAlignment(Pos.CENTER);
         VBox mainBox = new VBox(searchField,displayArea);
+        mainBox.setPrefWidth(1000);
+        mainBox.setSpacing(20);
         mainBox.setAlignment(Pos.CENTER);
         ourScene = new Scene(mainBox, 1000, 1000);
 
         searchButton.setOnAction(TitleScene.searchButtonClick());
         searchResults.setOnMouseClicked(TitleScene.getRecs());
+
     }
 
 
